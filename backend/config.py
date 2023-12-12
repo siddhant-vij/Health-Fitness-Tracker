82c96ce109284d89ca740919805489ed1b18fa83
Siddhant Vij <siddhantvij201@gmail.com>
2023-12-12 19:36:26 +0530

    Config File Updated - Email Notifs

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,8 @@ load_dotenv()
 class Config:
     NUTRITIONIX_APP_ID = os.getenv('NUTRITIONIX_APP_ID')
     NUTRITIONIX_API_KEY = os.getenv('NUTRITIONIX_API_KEY')
+
+    SMTP_SERVER = os.getenv('SMTP_SERVER')
+    SMTP_PORT = os.getenv('SMTP_PORT')
+    SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+    SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
